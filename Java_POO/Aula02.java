582fd7361a6ed80f9cb8c4235e1e3593d6500ca9
JuniorMelo <juniormelo.dev@gmail.com>
2022-05-08 12:58:10 -0300

    Add aulas de Java

---
+++ Java_POO/Aula02.java
@@ -0,0 +1,18 @@
+public class Aula02 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampar();
+
+        c1.status();
+        c1.rabiscar();
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "Java_POO";
+        c2.cor = "Preta";
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
+    }
+}
---
+++ Java_POO/Caneta.java
@@ -0,0 +1,28 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+    void rabiscar() {
+        if (this.tampada == true) {
+            System.out.println("Erro!! Não posso rabiscar..");
+        } else {
+            System.out.println("Estou rabiscando..");
+        }
+    }
+    void tampar() {
+        this.tampada = true;
+    }
+    void destampar() {
+        this.tampada = false;
+    }
+}
