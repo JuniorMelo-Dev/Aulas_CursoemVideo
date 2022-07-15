9a81fa595d45b75e6e618a248d9c2b7c4ab6acd5
JuniorMelo <juniormelo.dev@gmail.com>
2022-07-15 10:27:01 -0300

    :package:Add aula condicional pt1

---
+++ CursoJS/Aulas/ex008.js
@@ -0,0 +1,8 @@
+var velocidade = 55
+
+console.log(`A velocidade do seu veículo é ${velocidade}Km/h`)
+
+if (velocidade > 60) {  //consição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
