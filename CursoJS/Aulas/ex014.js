6c1febad50b8e8d48f8a75e31f90d1a2e2f36ce1
JuniorMelo <juniormelo.dev@gmail.com>
2022-07-17 09:09:07 -0300

    :up:atualização da aula

--- CursoJS/Aulas/ex014.js
+++ CursoJS/Aulas/ex014.js
@@ -1,7 +1,16 @@
 //Estrutura de repetição com teste lógico no inicio
 
 var contador = 1
-while (contador <= 6) {
-    console.log('Eai, mais um?')
-    contador++
-}
+    while (contador <= 6) {
+        console.log('Eai, mais um?')
+        contador++
+    }
+
+
+//Estrutura de repetição com teste lógico no final
+
+var contador = 1
+    do {
+        console.log('Eai, mais um?')
+        contador++
+    } while (contador <= 6)
